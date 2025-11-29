d94420bc0cddf1b80cb8424a7652a6e122e4e031
Barathramesh <barathramesh26@gmail.com>
2025-11-29 22:35:45 +0530

    Removal of code

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -1,18 +1,12 @@
 import express from 'express';
-import {
-  studentLogin,
-  teacherLogin,
-  studentRegister,
-  teacherRegister
-} from '../controllers/authController.js';
+import { studentLogin,teacherLogin } from '../controllers/authController.js';
 
 const router = express.Router();
 
 // Student routes
 router.post('/student/login', studentLogin);
-router.post('/student/register',studentRegister);
 
 // Teacher routes
 router.post('/teacher/login', teacherLogin);
-router.post('/teacher/register', teacherRegister);
+
 export default router;
