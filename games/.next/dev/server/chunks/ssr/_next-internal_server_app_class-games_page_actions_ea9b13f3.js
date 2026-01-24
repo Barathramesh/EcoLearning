db30ef71173f7ac8b3f63235224094c0f6147767
Barathramesh <barathramesh26@gmail.com>
2026-01-24 16:09:04 +0530

    Deleted unsed files

--- games/.next/dev/server/chunks/ssr/_next-internal_server_app_class-games_page_actions_ea9b13f3.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/class-games/page/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_class-games_page_actions_ea9b13f3.js.map
--- wall-of-frame-2/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import WallOfFrameBanner from "@/components/wall-of-frame-banner"
-
-export default function Home() {
-  return (
-    <main className="w-full min-h-screen bg-background">
-      <WallOfFrameBanner />
-    </main>
-  )
-}
